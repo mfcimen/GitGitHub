e7c8497ceb3872559003df0b3ab6693332b46fb6
Feyyaz <mfeyyazcimen@gmail.com>
2019-02-17 12:36:34 -0500

    commit 3

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -30,10 +30,21 @@ public class LoginTests {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        Assert.assertEquals(driver.getTitle(), "Web orders");
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
 
     }
 
+
+    @Test
+    public void logutTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+    }
 
 
     @AfterMethod
