2f925e6a775f5cfafb785d3884bf6f59aa02d864
mfcimen <42627170+mfcimen@users.noreply.github.com>
2019-02-17 13:24:32 -0500

    Added variable

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -30,9 +30,7 @@ public class LoginTests {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        
         String title = driver.getTitle();
-        
         Assert.assertEquals(title, "Web Orders");
 
     }
